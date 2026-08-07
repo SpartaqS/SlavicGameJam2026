92c8b7af80d66f96445c1abc2646601bcfa3e1be
Maciekxdabu <maciekxdabu@gmail.com>
2026-08-07 19:15:52 +0200

    Panel Interface and FuelControl test

---
+++ Chillnobyl/Assets/Scripts/Controls/FuelControl.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class FuelControl : MonoBehaviour, IParameterControlPanel
+{
+    [SerializeField] private float fuelPerPump = 10f;
+
+    public bool isMalfunctioning { get; private set; }
+
+    public Func<float> deltaOnClick
+    {
+        get {
+            return () =>
+            {
+                Debug.Log("Delta on Click in Fuel Control");
+                return fuelPerPump;
+            };
+        }
+    }
+
+    public Func<float> deltaOnState
+    {
+        get
+        {
+            return () =>
+            {
+                Debug.Log("Delta on State in Fuel Control");
+                return 0f;
+            };
+        }
+    }
+}
